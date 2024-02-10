7c5f78e40697b0e83cc24c05092551e443ecbcfe
Jeremy Kuehn <100542455+arkterra90@users.noreply.github.com>
2024-02-10 10:21:32 -0600

    updates to blog

--- district2/blog/urls.py
+++ district2/blog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
-app_name = 'website'
+app_name = 'blog'
 
 urlpatterns = [
+
+    path("<int:postID>/blog", views.blogpost, name="blogpost"),
+    path("blog", views.blogHome, name="blogHome")
 
 ]
--- district2/blog/views.py
+++ district2/blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def blogpost(request):
+    return
+
+def blogHome(request):
+    return render(request, "blog/index.html")
