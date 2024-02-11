8b3d1f6e01a4919623be99ca9396cb5ae1ec2e33
Jeremy Kuehn <100542455+arkterra90@users.noreply.github.com>
2024-02-10 20:49:40 -0600

    push for dep

--- district2/district2/settings.py
+++ district2/district2/settings.py
@@ -122,11 +122,13 @@ TIME_ZONE = 'America/Chicago'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-MEDIA_ROOT = BASE_DIR / 'media'
-MEDIA_URL = '/media/'
+
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
